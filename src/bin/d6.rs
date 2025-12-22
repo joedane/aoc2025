d5319be8986e80beedde5ca296ab8d5de5fe078a
Joseph Dane <joe.dane@gmail.com>
2025-12-21 14:33:21 -1000

    Day 6 completed.

--- src/bin/d6.rs
+++ src/bin/d6.rs
@@ -1,5 +1,3 @@
-use std::f32::consts::E;
-
 fn part1(input: &str) {
     let mut data: Vec<Vec<&str>> = vec![];
 
@@ -34,35 +32,93 @@ fn part1(input: &str) {
     println!("{}", total);
 }
 
+struct Data<'a> {
+    data: &'a [u8],
+    column_sizes: Vec<usize>,
+    num_columns: usize,
+    num_rows: usize,
+    line_len: usize,
+}
+
+impl<'a> Data<'a> {
+    fn new(data: &'a str) -> Self {
+        let mut line_count = 0;
+        let mut line_len = 0;
+        let column_sizes: Vec<usize> = data
+            .lines()
+            .map(|line| {
+                line_count += 1;
+                line_len = line.len() + 1; //assume this is constant.  add one for the newline stripped by lines()
+                line.split_whitespace()
+                    .map(|s| s.len())
+                    .collect::<Vec<usize>>()
+            })
+            .reduce(|acc, e| {
+                acc.iter()
+                    .zip(e.iter())
+                    .map(|(a, b)| std::cmp::max(*a, *b))
+                    .collect()
+            })
+            .unwrap();
+        let num_columns = column_sizes.len();
+        Self {
+            data: data.as_bytes(),
+            column_sizes,
+            num_columns,
+            num_rows: line_count,
+            line_len,
+        }
+    }
+
+    fn process_column(&self, col: usize) -> u64 {
+        let column_start_index = self.column_start_index(col);
+        let mut operands: Vec<u64> = vec![];
+        let mut adding = true;
+        for c in 0..self.column_sizes[col] {
+            let i = column_start_index + self.column_sizes[col] - 1 - c;
+            let mut char_value: u64 = 0;
+            for row in 0..self.num_rows {
+                match self.data[i + row * self.line_len] {
+                    digit @ 48..=57 => {
+                        char_value = (char_value * 10) + digit as u64 - 48u64;
+                    }
+                    42 => {
+                        adding = false;
+                    }
+                    43 | 32 => {} // plus sign or space
+                    bad @ _ => panic!("invalid character '{}'", bad),
+                }
+            }
+            operands.push(char_value)
+        }
+        operands
+            .into_iter()
+            .reduce(|acc, x| if adding { acc + x } else { acc * x })
+            .unwrap()
+    }
+
+    fn column_start_index(&self, col: usize) -> usize {
+        self.column_sizes[0..col].iter().map(|s| s + 1).sum()
+    }
+}
+
 fn part2(input: &str) {
-    let lines: Vec<&str> = input.lines().collect();
-    let max_widths: Vec<usize> = lines
-        .iter()
-        .map(|line| {
-            line.split_whitespace()
-                .map(|s| s.len())
-                .collect::<Vec<usize>>()
-        })
-        .reduce(|acc, e| {
-            println!("acc: {:?}, e: {:?}", acc, e);
-            acc.iter()
-                .zip(e.iter())
-                .inspect(|e| println!("{:?}", e))
-                .map(|(a, b)| std::cmp::max(*a, *b))
-                .collect()
-        })
-        .unwrap();
-    println!("{:?}", max_widths);
+    let data = Data::new(input);
+    let mut total = 0;
+    for c in 0..data.num_columns {
+        total += data.process_column(c);
+    }
+
+    println!("{}", total);
 }
 fn main() {
-    let input = TEST;
-    // let input = std::fs::read_to_string("input/d06.txt").unwrap();
+    //let input = TEST;
+    let input = std::fs::read_to_string("input/d06.txt").unwrap();
     part2(&input);
 }
 
 #[rustfmt::skip]
-const TEST: &str = r#"
-123 328  51 64 
+const TEST: &str = r#"123 328  51 64 
  45 64  387 23 
   6 98  215 314
 *   +   *   +  
